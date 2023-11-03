e737b39301fb895c1796f11cb2c976d7b653bf86
Luis Filipe Guedes Mota <filipemot@gmail.com>
2023-11-03 18:00:30 -0300

    Aula 2 - Componentização

--- alura-studies/src/components/Lista/index.tsx
+++ alura-studies/src/components/Lista/index.tsx
@@ -3,10 +3,13 @@ import React from "react";
 function Lista() {
     const tarefas = [{
         tarefa: 'React',
-          tempo: '02:00:00'
+        tempo: '02:00:00'
       }, {
         tarefa: 'Javascript',
-          tempo: '01:00:00'
+        tempo: '01:00:00'
+      }, {
+        tarefa: 'Typescript',
+        tempo: '03:00:00'
       }]
     return (
       <aside>

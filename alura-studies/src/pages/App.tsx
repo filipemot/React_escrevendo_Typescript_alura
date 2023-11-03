cec64b0fdff2331c164d3e5e08c25d95ccd140dd
Luis Filipe Guedes Mota <filipemot@gmail.com>
2023-11-03 19:12:47 -0300

    Aula 4 - Props

--- alura-studies/src/pages/App.tsx
+++ alura-studies/src/pages/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import style from './App.module.scss'
+import Cronometro from '../components/Cronometro';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className={style.AppStyle}>
       <Formulario />
       <Lista/>
+      <Cronometro/>
     </div>
   );
 }

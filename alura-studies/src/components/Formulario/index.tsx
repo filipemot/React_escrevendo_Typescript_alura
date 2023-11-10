0bd797a6c5cd65c99f0e604ffd4c675c0097f449
Luis Filipe Guedes Mota <filipemot@gmail.com>
2023-11-10 18:57:48 -0300

    Aula 5 - Atualizando a tarefa

--- alura-studies/src/components/Formulario/index.tsx
+++ alura-studies/src/components/Formulario/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Botao from '../Botao';
 import style from './Formulario.module.scss';
 import { ITarefa } from '../types/tarefa';
+import { v4 as uuidv4 } from 'uuid';
 
 class Formulario extends React.Component<{setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>}> {
   state = {
@@ -10,8 +11,22 @@ class Formulario extends React.Component<{setTarefas: React.Dispatch<React.SetSt
   }
   adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
     evento.preventDefault();
-    this.props.setTarefas(tarefasAntigas => [... tarefasAntigas, this.state])
+    this.props.setTarefas(tarefasAntigas =>
+       [
+        ...tarefasAntigas, 
+        {
+          ...this.state,
+          selecionado: false,
+          completado: false,
+          id: uuidv4()
+        }
+      ]
+    );
     console.log(this.state)
+    this.setState({
+      tarefa: "",
+      tempo: "00:00:00"
+    })
   }
   render() {
     return (
